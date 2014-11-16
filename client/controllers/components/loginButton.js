be45bee8f7815fb17cc61e38aa3327b2a3290f46
Ian Sinnott <ian@iansinnott.com>
2014-11-16 18:21:46 +0800

    Add menu dropdown

--- client/controllers/components/loginButton.js
+++ client/controllers/components/loginButton.js
@@ -1,6 +1,13 @@
 Template.loginButton.events({
+
+  'click .dropdown .toggle': function(e, template) {
+    e.preventDefault();
+    template.$('.dropdown').toggleClass('open');
+  },
+
   'click .logout': function(e) {
     e.preventDefault();
     Meteor.logout();
   }
+
 });
